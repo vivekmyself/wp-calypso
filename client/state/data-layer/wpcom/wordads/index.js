6dd88e211612dc2835516e7f43019e0ae1b5c42d
Richard C Lations II <contact@rclations.com>
2018-07-16 18:06:13 -0400

    WordAds: stats scaffolding in redux & data-layer

--- client/state/data-layer/wpcom/wordads/index.js
+++ client/state/data-layer/wpcom/wordads/index.js
@@ -6,6 +6,7 @@
 
 import { mergeHandlers } from 'state/action-watchers/utils';
 import earnings from './earnings';
+import stats from './stats';
 import status from './status';
 
-export default mergeHandlers( earnings, status );
+export default mergeHandlers( earnings, stats, status );
