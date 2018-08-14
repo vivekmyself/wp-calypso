d988b3d3b934e1e905629d2682b33c91db2bda9d
Markus Dobmann <markus@mpnjs.com>
2018-08-14 16:00:07 +0200

    store: alias import to 'store/dist/store.modern'

--- webpack.config.js
+++ webpack.config.js
@@ -223,6 +223,7 @@ function getWebpackConfig( { extensionName = '' } = {}, argv ) {
 					'react-virtualized': 'react-virtualized/dist/commonjs',
 					'social-logos/example': 'social-logos/build/example',
 					debug: path.resolve( __dirname, 'node_modules/debug' ),
+					store: 'store/dist/store.modern',
 				},
 				getAliasesForExtensions()
 			),
